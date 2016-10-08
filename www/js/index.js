bfdd9bc8884c47f00f35ecfe00c2295dc84056f8
Max <madramsdell@gmail.com>
2016-10-08 17:21:34 -0600

    NCF scanner/Server Side Retrieve is part of client side. Index.js is entire server side.

---
+++ www/js/index.js
@@ -0,0 +1,31 @@
+bases = [
+    {color:'red', nfcID:'b00'},
+    {color:'red', nfcID:'b01'},
+    {color:'red', nfcID:'b02'},
+    {color:'red', nfcID:'b03'},
+    {color:'red', nfcID:'b04'},
+    {color:'red', nfcID:'b05'},
+    {color:'red', nfcID:'b06'}
+];
+
+users = [
+     {color:'red', id:'madram'},
+     {color:'blue', id:'meikul'}
+];
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+io.on('connection', function(socket){
+  socket.on('BRequest', function(req){
+      io.emit("BSend", bases);
+  });
+});
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ www/js/retrieve.js
@@ -0,0 +1,11 @@
+nfc.addMimeTypeListener('flaggy', function(nfcEvent){
+    var records = nfcEvent.tagData;
+
+    for (var i = 0; i < records.length; i++){
+        var record = records[i];
+        p = document.createElement('p');
+        p.innerHTML = nfc.bytesToString(record.payload);
+        display.appendChild(p);
+    }
+});
+
