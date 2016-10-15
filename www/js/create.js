0501ef6a99f2e9efc365fa603348a7542c3ac9f1
Michael Harris <meikul.harris@gmail.com>
2016-10-15 17:51:08 -0600

    color palette and packaging data for server (no sending)

--- www/js/create.js
+++ www/js/create.js
@@ -1,23 +1,53 @@
+var baseName = 0;
+var bases = [];
 $(document).ready(function(){
-	$(document).on('vmousedown',function(event){
-		switch(event.target.id){
-			case "base":
-				editBase(event.target);
-				break;
-			case "baseHeader":
-				createBase(event.clientX, event.clientY);
-				break;
-			case "delete":
-				$(event.target.parentNode.parentNode).remove();
-		}
+	$(document).on('tap', function(event){
+		event.preventDefault();
+		var id = event.target.id;
+		if(id=="base")
+			editBase(event.target);
+		if(id=="baseHeader")
+			createBase(event.clientX, event.clientY);
+		if(id=="delete")
+			deleteBase(event.target);
+		if(id=="color")
+			appendPalette(event.target);
+		if(id=="redSq"||id=="yellowSq"||id=="greenSq"||id=="blueSq")
+			colorBase($(event.target).css('background'), event.target.parentNode.parentNode);
 	});
 });
-
 function createBase(x, y){
-	console.log("create base");
-	$('#baseHeader').after('<div id="base" style="left:'+(x-30)+'px; top:'+(y-30)+'px;"></div>');
+	$('#baseHeader').after('<div id="base" props="name team" style="left:'+(x-30)+'px; top:'+(y-30)+'px;"></div>');
 }
 function editBase(base){
-	console.log("edit base: "+base.id+", "+base.style.left+", "+base.style.top);
-	$(base).append('<div id="editMenu" style="left:-10px; top:-30px;" class="text"><div id="color"></div><div id="delete">T</div></div>');
+	if(base.hasChildNodes())
+		$(base).children().remove();
+	else
+		$(base).append('<div id="editMenu" class="text"><div id="color"></div><div id="delete">T</div></div>');
+}
+function deleteBase(base){
+	$(base.parentNode.parentNode).remove();
+}
+function appendPalette(symbol){
+	$(symbol.parentNode).append('<div id="redSq"></div><div id="yellowSq"></div><div id="greenSq"></div><div id="blueSq"></div>');
+}
+function colorBase(color, base){
+	$(base).css('background', color);
+	$(base).children().remove();
+}
+function bundleBases(){
+	$('#base').forEach(function(){
+		var propsStr = $(this).attr("props");
+		var props = propsStr.split(" ");
+		props.forEach(function(val, i){
+			if(/\d/.test(val)) //if value is number
+				props[i] = parseInt(val);
+		});
+		bases.push(new Base(props));
+	});
+	//send bases to server
+}
+function Base(props){
+	this.name = props[0];
+	this.team = props[1];
 }
--- www/js/script.js
+++ www/js/script.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
 	$('.button').click(function(){
 		window.location.href = $(this).attr('href');
 	});
-	nfc.addMimeTypeListener('flaggy', function(nfcEvent){
-    	var records = nfcEvent.tagData;
+	// nfc.addMimeTypeListener('flaggy', function(nfcEvent){
+ //    	var records = nfcEvent.tagData;
 
-    	for (var i = 0; i < records.length; i++){
-        	var record = records[i];
-        	socket.emit('nfcID', nfc.bytesToString(record.payload));
-    	}
-	});
+ //    	for (var i = 0; i < records.length; i++){
+ //        	var record = records[i];
+ //        	socket.emit('nfcID', nfc.bytesToString(record.payload));
+ //    	}
+	// });
 
 	// var socket = io();
 	// socket.on('BSend', function(bases){
